3d17e2b79aa9b884e5693c21fb55b052dc423bfd
jonathan.byrne <jonathan.byrne@yahoo.com>
2019-03-05 08:02:25 -0500

    Added XML Injector test

---
+++ ByrneLabs.TestoRoboto.HttpServices.Tests/JsonMutators/XmlInjectorTest.cs
@@ -0,0 +1,14 @@
+﻿using ByrneLabs.TestoRoboto.HttpServices.JsonMutators;
+using Xunit;
+
+namespace ByrneLabs.TestoRoboto.HttpServices.Tests.JsonMutators
+{
+    public class SqlInjectorTest : MutatorTest
+    {
+        [Fact]
+        public void TestSqlInjector()
+        {
+            TestMessageCountReturned<SqlInjector>(377);
+        }
+    }
+}
