e89b61782c6bfb73df3e1e9c64913d47ee2ffc3e
jonathan.byrne <jonathan.byrne@yahoo.com>
2019-03-18 13:18:00 -0400

    Made minor progress on initial implementation of UI

--- ByrneLabs.TestoRoboto.Desktop/RequestMessage.xaml.cs
+++ ByrneLabs.TestoRoboto.Desktop/RequestMessage.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace ByrneLabs.TestoRoboto.Desktop
+﻿using ByrneLabs.TestoRoboto.Desktop.ViewModels;
+
+namespace ByrneLabs.TestoRoboto.Desktop
 {
     public partial class RequestMessage
     {
         public RequestMessage()
         {
             InitializeComponent();
+
+            var requestMessageViewModel = new RequestMessageViewModel();
+            requestMessageViewModel.Name = "name";
+            requestMessageViewModel.QueryParameters.Add(new KeyValueViewModel { Key = "key1", Value = "value1", Description = "description"});
+
+            DataContext = requestMessageViewModel;
         }
     }
 }
--- ByrneLabs.TestoRoboto.Desktop/ViewModels/RequestMessageViewModel.cs
+++ ByrneLabs.TestoRoboto.Desktop/ViewModels/RequestMessageViewModel.cs
@@ -7,27 +7,29 @@ namespace ByrneLabs.TestoRoboto.Desktop.ViewModels
     {
         public string AuthenticationType { get; set; }
 
-        public IEnumerable<string> AuthenticationTypes { get; set; }
+        public IEnumerable<string> AuthenticationTypesToChooseFrom { get; set; }
 
         public object AuthenticationViewModel { get; set; }
 
         public string BodyType { get; set; }
 
-        public IEnumerable<string> BodyTypes { get; set; }
+        public IEnumerable<string> BodyTypes { get; set; } = new List<string>();
 
         public object BodyViewModel { get; set; }
 
         public string ContentType { get; set; }
 
-        public IEnumerable<string> ContentTypes { get; set; }
+        public IEnumerable<string> ContentTypesToChooseFrom { get; set; } = new List<string>();
 
-        public IList<KeyValueViewModel> Headers { get; set; }
+        public IList<KeyValueViewModel> Headers { get; set; } = new List<KeyValueViewModel>();
 
         public string HttpMethod { get; set; }
 
-        public IEnumerable<string> HttpMethods { get; set; }
+        public IEnumerable<string> HttpMethodsToChooseFrom { get; set; } = new List<string>();
 
         public string Name { get; set; }
+
+        public IList<KeyValueViewModel> QueryParameters { get; set; } = new List<KeyValueViewModel>();
 
         public string Url { get; set; }
     }
