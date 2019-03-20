f15696e21189277713adbaccff36fef955b1e6d4
jonathan.byrne <jonathan.byrne@yahoo.com>
2019-03-20 09:59:14 -0400

    Renamed handler classes to make more sense

---
+++ ByrneLabs.TestoRoboto.Crawler/PageItems/InputHandlerBase.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace ByrneLabs.TestoRoboto.Crawler.PageItems
+{
+    public abstract class InputHandlerBase : DataInputHandler
+    {
+        public abstract IEnumerable<string> InputTypes { get; }
+
+        public override bool CanHandle(PageItem pageItem) => pageItem.Tag == "input" && InputTypes.Contains(pageItem.Type) && !(string.IsNullOrWhiteSpace(pageItem.Id) && string.IsNullOrWhiteSpace(pageItem.Name));
+
+        public override void FillInput(RemoteWebDriver webDriver, PageItem input)
+        {
+            var element = FindElement(webDriver, input);
+            if (!element.Displayed || !element.Enabled)
+            {
+                return;
+            }
+
+            var sampleText = GetSampleText(element);
+
+            /*
+             * We might have already filled in this field and we only want to do it once
+             */
+            if (element.GetProperty("value") != sampleText)
+            {
+                try
+                {
+                    element.Clear();
+                    element.SendKeys(sampleText);
+                }
+                catch (ElementNotInteractableException)
+                {
+                    /*
+                     * Unclear how this happens if it is both displayed and enabled
+                     */
+                }
+            }
+        }
+
+        public override IEnumerable<PageItem> FindDataInputs(RemoteWebDriver webDriver)
+        {
+            var typeCheck = string.Join(" or ", InputTypes.Select(inputType => $"@type='{inputType}'"));
+            return FindElementsByXPath(webDriver, "//input[" + typeCheck + "]").Select(webElement =>
+                PageItem.CreatePageItem(
+                    webElement.GetProperty("class"),
+                    null,
+                    webElement.GetProperty("id"),
+                    webElement.GetProperty("name"),
+                    null,
+                    webElement.GetProperty("title"),
+                    webElement.TagName,
+                    webElement.GetProperty("type"),
+                    webElement.GetProperty("value"),
+                    Identifier)
+            ).Where(pageItem => pageItem != null).ToArray();
+        }
+
+        protected abstract string GetSampleText(IWebElement webElement);
+    }
+}
---
+++ ByrneLabs.TestoRoboto.Crawler/PageItems/ItemHandlerBase.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace ByrneLabs.TestoRoboto.Crawler.PageItems
+{
+    public abstract class ItemHandlerBase
+    {
+        public abstract string Identifier { get; }
+
+        protected static IWebElement FindElement(RemoteWebDriver webDriver, PageItem pageItem)
+        {
+            var attributeFilterList = new List<string>();
+
+            if (!string.IsNullOrWhiteSpace(pageItem.Class))
+            {
+                attributeFilterList.Add($"@class='{pageItem.Class}'");
+            }
+
+            if (!string.IsNullOrWhiteSpace(pageItem.Id))
+            {
+                attributeFilterList.Add($"@id='{pageItem.Id}'");
+            }
+
+            if (!string.IsNullOrWhiteSpace(pageItem.Name))
+            {
+                attributeFilterList.Add($"@name='{pageItem.Name}'");
+            }
+
+            if (!string.IsNullOrWhiteSpace(pageItem.Title) && !pageItem.Title.Contains("'"))
+            {
+                attributeFilterList.Add($"@title='{pageItem.Title}'");
+            }
+
+            if (!string.IsNullOrWhiteSpace(pageItem.Type))
+            {
+                attributeFilterList.Add($"@type='{pageItem.Type}'");
+            }
+
+            if (!string.IsNullOrWhiteSpace(pageItem.OnClick) && !pageItem.OnClick.Contains("'"))
+            {
+                attributeFilterList.Add($"@onclick='{pageItem.OnClick}'");
+            }
+
+            var attributeFilter = string.Join(" and ", attributeFilterList);
+
+            var elements = FindElementsByXPath(webDriver, $"//{pageItem.Tag}[{attributeFilter}]");
+            if (!elements.Any())
+            {
+                throw new ItemNotFoundException(pageItem);
+            }
+
+            if (elements.Count > 1)
+            {
+                //todo: it might be better to throw an exception here but this is easier
+                return elements.First();
+            }
+
+            return elements.Single();
+        }
+
+        protected static IReadOnlyCollection<IWebElement> FindElementsByXPath(RemoteWebDriver webDriver, string xPath)=> webDriver.FindElementsByXPath(xPath).Select(element => new SmartWebElement(element)).ToList().AsReadOnly();
+
+        public abstract bool CanHandle(PageItem pageItem);
+    }
+}
